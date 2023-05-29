23d504deebcdad5b158324b950b5ecbe1fce7ed8
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-05-29 01:32:22 -0300

    Cadastro do Ponto de Coleta

--- src/database/migrations/02_create_point_items.ts
+++ src/database/migrations/02_create_point_items.ts
@@ -1,7 +1,20 @@
-export async function up(){
+import Knex from 'knex';
 
+export async function up(knex: ReturnType<typeof Knex>) {
+    return knex.schema.createTable('point_items', table =>{
+        table.increments('id').primary();
+        table.integer('point_id')
+        .notNullable()
+        .references('id')
+        .inTable('points');
+
+        table.integer('item_id')
+        .notNullable()
+        .references('id')
+        .inTable('items');
+    })
 }
 
-export async function down(){
-    
+export async function down(knex: ReturnType<typeof Knex>) {
+    return knex.schema.dropTable('point_items');
 }
